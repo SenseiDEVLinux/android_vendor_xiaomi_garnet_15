c3b6c310e9af716da206cc67de53415efc5a70b4
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-03-09 16:12:57 +0300

    Update garnet-vendor.mk

--- garnet-vendor.mk
+++ garnet-vendor.mk
@@ -979,6 +979,7 @@ PRODUCT_PACKAGES += \
     libswgamedap \
     libswvqe \
     vendor.dolby.hardware.dms@2.0 \
+    vendor.qti.hardware.AGMIPC@1.0-impl \
     vendor.qti.qspmhal@1.0 \
     android.hardware.secure_element@1.0-impl \
     btaudio_offload_if \
